d8d6232d39dbc0fa9aaf1f5f2d09427d6ed5f920
Kenneth Matovu <matovken@gmail.com>
2013-05-27 13:27:48 +0300

    added initial categories, made action nullable

--- message_classifier/migrations/0001_initial.py
+++ message_classifier/migrations/0001_initial.py
@@ -3,10 +3,10 @@ import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db.utils import DatabaseError
+from django.core import management
 
 
 class Migration(SchemaMigration):
-
     def forwards(self, orm):
         try:
             # Adding model 'IbmCategory'
@@ -15,6 +15,7 @@ class Migration(SchemaMigration):
                 ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ))
             db.send_create_signal('message_classifier', ['IbmCategory'])
+            management.call_command('loaddata', 'initial_categories.json')
 
             # Adding model 'IbmAction'
             db.create_table('ibm_action', (
@@ -25,10 +26,13 @@ class Migration(SchemaMigration):
 
             # Adding model 'IbmMsgCategory'
             db.create_table('ibm_msg_category', (
-                ('msg', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['rapidsms_httprouter.Message'], primary_key=True)),
-                ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['message_classifier.IbmCategory'])),
+                ('msg', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['rapidsms_httprouter.Message'],
+                                                                              primary_key=True)),
+                ('category',
+                 self.gf('django.db.models.fields.related.ForeignKey')(to=orm['message_classifier.IbmCategory'])),
                 ('score', self.gf('django.db.models.fields.FloatField')()),
-                ('action', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['message_classifier.IbmAction'])),
+                ('action', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['message_classifier.IbmAction'],
+                                                                                 null=True)),
             ))
             db.send_create_signal('message_classifier', ['IbmMsgCategory'])
         except DatabaseError, e:
@@ -54,12 +58,15 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Group'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [],
+                            {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
         'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')",
+                     'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'content_type': (
+                'django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
@@ -68,7 +75,8 @@ class Migration(SchemaMigration):
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [],
+                       {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -76,11 +84,13 @@ class Migration(SchemaMigration):
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [],
+                                 {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
         'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)",
+                     'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -90,23 +100,32 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Location'},
             'code': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
+            'is_active': (
+                'django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
             'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'parent_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'parent_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
-            'point': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['locations.Point']", 'null': 'True', 'blank': 'True'}),
+            'parent_type': ('django.db.models.fields.related.ForeignKey', [],
+                            {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'point': ('django.db.models.fields.related.ForeignKey', [],
+                      {'to': "orm['locations.Point']", 'null': 'True', 'blank': 'True'}),
             'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'status': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
+            'status': (
+                'django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
             'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['locations.Location']"}),
-            'type': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'locations'", 'null': 'True', 'to': "orm['locations.LocationType']"})
+            'tree_parent': ('django.db.models.fields.related.ForeignKey', [],
+                            {'blank': 'True', 'related_name': "'children'", 'null': 'True',
+                             'to': "orm['locations.Location']"}),
+            'type': ('django.db.models.fields.related.ForeignKey', [],
+                     {'blank': 'True', 'related_name': "'locations'", 'null': 'True',
+                      'to': "orm['locations.LocationType']"})
         },
         'locations.locationtype': {
             'Meta': {'object_name': 'LocationType'},
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'primary_key': 'True', 'db_index': 'True'})
+            'slug': ('django.db.models.fields.SlugField', [],
+                     {'unique': 'True', 'max_length': '50', 'primary_key': 'True', 'db_index': 'True'})
         },
         'locations.point': {
             'Meta': {'object_name': 'Point'},
@@ -127,8 +146,10 @@ class Migration(SchemaMigration):
         'message_classifier.ibmmsgcategory': {
             'Meta': {'object_name': 'IbmMsgCategory', 'db_table': "'ibm_msg_category'"},
             'action': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['message_classifier.IbmAction']"}),
-            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['message_classifier.IbmCategory']"}),
-            'msg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rapidsms_httprouter.Message']", 'primary_key': 'True'}),
+            'category': (
+                'django.db.models.fields.related.ForeignKey', [], {'to': "orm['message_classifier.IbmCategory']"}),
+            'msg': ('django.db.models.fields.related.ForeignKey', [],
+                    {'to': "orm['rapidsms_httprouter.Message']", 'primary_key': 'True'}),
             'score': ('django.db.models.fields.FloatField', [], {})
         },
         'rapidsms.backend': {
@@ -139,7 +160,8 @@ class Migration(SchemaMigration):
         'rapidsms.connection': {
             'Meta': {'object_name': 'Connection'},
             'backend': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rapidsms.Backend']"}),
-            'contact': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rapidsms.Contact']", 'null': 'True', 'blank': 'True'}),
+            'contact': ('django.db.models.fields.related.ForeignKey', [],
+                        {'to': "orm['rapidsms.Contact']", 'null': 'True', 'blank': 'True'}),
             'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'identity': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -151,30 +173,46 @@ class Migration(SchemaMigration):
             'birthdate': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
             'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'gender': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['auth.Group']", 'null': 'True', 'blank': 'True'}),
-            'health_facility': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [],
+                       {'symmetrical': 'False', 'to': "orm['auth.Group']", 'null': 'True', 'blank': 'True'}),
+            'health_facility': (
+                'django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_caregiver': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'language': ('django.db.models.fields.CharField', [], {'max_length': '6', 'blank': 'True'}),
             'modified_on': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
-            'occupation': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
-            'reporting_location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['locations.Location']", 'null': 'True', 'blank': 'True'}),
-            'subcounty': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'subcounties'", 'null': 'True', 'to': "orm['locations.Location']"}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'contact'", 'unique': 'True', 'null': 'True', 'to': "orm['auth.User']"}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'village': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'villagers'", 'null': 'True', 'to': "orm['locations.Location']"}),
-            'village_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+            'occupation': (
+                'django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'reporting_location': ('django.db.models.fields.related.ForeignKey', [],
+                                   {'to': "orm['locations.Location']", 'null': 'True', 'blank': 'True'}),
+            'subcounty': ('django.db.models.fields.related.ForeignKey', [],
+                          {'blank': 'True', 'related_name': "'subcounties'", 'null': 'True',
+                           'to': "orm['locations.Location']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [],
+                     {'blank': 'True', 'related_name': "'contact'", 'unique': 'True', 'null': 'True',
+                      'to': "orm['auth.User']"}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [],
+                                 {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'village': ('django.db.models.fields.related.ForeignKey', [],
+                        {'blank': 'True', 'related_name': "'villagers'", 'null': 'True',
+                         'to': "orm['locations.Location']"}),
+            'village_name': (
+                'django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
         },
         'rapidsms_httprouter.message': {
             'Meta': {'object_name': 'Message'},
             'application': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
-            'batch': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'messages'", 'null': 'True', 'to': "orm['rapidsms_httprouter.MessageBatch']"}),
-            'connection': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'messages'", 'to': "orm['rapidsms.Connection']"}),
+            'batch': ('django.db.models.fields.related.ForeignKey', [],
+                      {'related_name': "'messages'", 'null': 'True', 'to': "orm['rapidsms_httprouter.MessageBatch']"}),
+            'connection': ('django.db.models.fields.related.ForeignKey', [],
+                           {'related_name': "'messages'", 'to': "orm['rapidsms.Connection']"}),
             'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'direction': ('django.db.models.fields.CharField', [], {'max_length': '1', 'db_index': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'in_response_to': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'responses'", 'null': 'True', 'to': "orm['rapidsms_httprouter.Message']"}),
+            'in_response_to': ('django.db.models.fields.related.ForeignKey', [],
+                               {'related_name': "'responses'", 'null': 'True',
+                                'to': "orm['rapidsms_httprouter.Message']"}),
             'priority': ('django.db.models.fields.IntegerField', [], {'default': '10', 'db_index': 'True'}),
             'status': ('django.db.models.fields.CharField', [], {'max_length': '1', 'db_index': 'True'}),
             'text': ('django.db.models.fields.TextField', [], {'db_index': 'True'})
